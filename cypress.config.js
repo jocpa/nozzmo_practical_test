58dc1ed47ecdaf42cbd0f64d4f36adac3102f38a
Pablo Arzu <jptomas38@gmail.com>
2024-03-12 16:01:53 -0600

    Agregando reportes con cypress-mocha-awesome

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,20 @@
-const { defineConfig } = require("cypress");
+const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
+  reporter: 'cypress-mochawesome-reporter',
+  reporterOptions: { //add
+    charts: true,
+    reportPageTitle: 'custom-title',
+    embeddedScreenshots: true,
+    inlineAssets: true,
+    saveAllAttempts: false,
+    reportPageTitle: "Orange Test",
+    embeddedScreenshots: true,
+  },
+
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      require('cypress-mochawesome-reporter/plugin')(on);
     },
   },
 });
